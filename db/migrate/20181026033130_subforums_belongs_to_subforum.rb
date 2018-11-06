7904c2b6752f0c6b79452d1b4ddd69febd078dc3
ZXeRRinor <antpotapov51@gmail.com>
2018-11-06 17:44:39 +0300

    Revert "Migration fix try"
    This reverts commit 377e056

--- db/migrate/20181026033130_subforums_belongs_to_subforum.rb
+++
@@ -1,7 +0,0 @@
-class SubforumsBelongsToSubforum < ActiveRecord::Migration[5.2]
-  def change
-    change_table :subforums do |t|
-      t.belongs_to :subforum, index: true
-    end
-  end
-end
--- db/migrate/20181026203947_messages_belongs_to_user_and_post.rb
+++
@@ -1,8 +0,0 @@
-class MessagesBelongsToUserAndPost < ActiveRecord::Migration[5.2]
-  def change
-    change_table :messages do |t|
-      t.belongs_to :post, index: true
-      t.belongs_to :user, index: true
-    end
-  end
-end
