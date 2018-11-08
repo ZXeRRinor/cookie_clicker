fe84d4c6ea7f7d28f689603ee9d1eb50277a7d3e
ZXeRRinor <antpotapov51@gmail.com>
2018-11-08 11:46:14 +0300

    Service for game controller

---
+++ app/services/game.rb
@@ -0,0 +1,3 @@
+module Game
+  PRODUCER_LIST = ['Auto Clicker', 'Auto Oven', 'Cookie Farm', 'Cookie Factory', 'Cookie Reactor', 'Cookie Materialiser', 'Quantum Cookie Singularity', 'Admin Cookie Creator']
+end
