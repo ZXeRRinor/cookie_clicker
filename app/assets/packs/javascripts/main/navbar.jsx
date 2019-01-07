a1d459c0b309a549c21c42340ae8b26e1652f095
ZXeRRinor <antpotapov51@gmail.com>
2019-01-07 20:49:31 +0300

    Added buttons to navbar

--- app/assets/packs/javascripts/main/navbar.jsx
+++ app/assets/packs/javascripts/main/navbar.jsx
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import {connect} from 'react-redux';
-import RegisterModal from "./modals/register_modal";
-import LoginModal from "./modals/login_modal";
-import {Menu} from 'antd';
+//import RegisterModal from "./modals/register_modal";
+//import LoginModal from "./modals/login_modal";
+import {Menu, Icon, Button} from 'antd';
 
 class Navbar extends Component {
 
@@ -26,7 +26,7 @@ class Navbar extends Component {
 
     render() {
         return (
-            <div>
+            <div style={{width: 256}}>
                 <Button type="primary" onClick={this.toggleCollapsed} style={{marginBottom: 16}}>
                     <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}/>
                 </Button>
@@ -36,130 +36,54 @@ class Navbar extends Component {
                     theme="dark"
                     inlineCollapsed={this.state.collapsed}
                 >
+                    <Menu.Item key="home">
+                        <Link to="/" className="nav-link">
+
+                            <span>Home</span>
+                        </Link>
+                    </Menu.Item>
                     <Menu.Item key="dictionary">
-                        <Icon type="interation" />
-                        <span>Dictionary</span>
+                        <Link to="" className="nav-link">
+
+                            <span>Dictionary</span>
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="notifications">
+                        <Link to="" className="nav-link">
+
+                            <span>Notifications</span>
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="my_discussions">
+                        <Link to="" className="nav-link">
+
+                            <span>My discussions</span>
+                        </Link>
                     </Menu.Item>
                     <Menu.Item key="messages">
-                        <Icon type="message" />
-                        <span>Messages</span>
+                        <Link to="" className="nav-link">
+
+                            <span>Messages</span>
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="settings">
+                        <Link to="" className="nav-link">
+
+                            <span>Settings</span>
+                        </Link>
                     </Menu.Item>
                     {this.state.permissions >= 2 ?
-                        <Menu.Item key="admin">
-                            <Link to="/admin/panel/" className="nav-link">Administrating</Link>
+                        <Menu.Item key="administrating">
+                            <Link to="/admin/panel/" className="nav-link">
+
+                                <span>Administrating</span>
+                            </Link>
                         </Menu.Item>
                         : ''
                     }
                 </Menu>
-                <Menu
-                    onClick={this.handleClick}
-                    selectedKeys={[this.state.current]}
-                    mode="horizontal"
-                >
-                    <Menu.Item key="dict">
-                        <Link to="/forum/0/" className="nav-link">Forum</Link>
-                    </Menu.Item>
-                </Menu>
             </div>
         );
-        {/*<div>*/
-        }
-        {/*<nav className="navbar sticky-top navbar-expand-lg bg-dark">*/
-        }
-        {/*<Link to='/' className="navbar-brand">Dictionary</Link>*/
-        }
-        {/*<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"*/
-        }
-        {/*aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">*/
-        }
-        {/*<span className="navbar-toggler-icon"></span>*/
-        }
-        {/*</button>*/
-        }
-        {/*<div className="collapse navbar-collapse" id="navbarNav">*/
-        }
-        {/*<ul className="navbar-nav">*/
-        }
-        {/*<li className="nav-item">*/
-        }
-        {/*<Link to="/forum/0/" className="nav-link">Forum</Link>*/
-        }
-        {/*</li>*/
-        }
-        {/*{*/
-        }
-        {/*}*/
-        }
-        {/*</ul>*/
-        }
-        {/*{*/
-        }
-        {/*this.state.current_user !== null ?*/
-        }
-        {/*<ul className="navbar-nav right_block row">*/
-        }
-        {/*<li>*/
-        }
-        {/*<RegisterModal/>*/
-        }
-        {/*</li>*/
-        }
-        {/*<span className="col-1"/>*/
-        }
-        {/*<li>*/
-        }
-        {/*<LoginModal/>*/
-        }
-        {/*</li>*/
-        }
-        {/*</ul>*/
-        }
-        {/*:*/
-        }
-        {/*<ul className="navbar-nav right_block"/>*/
-        }
-        {/*}*/
-        }
-        {/*{*/
-        }
-        {/*//<*/
-        }
-        {/*//% if current_user %>*/
-        }
-        {/*//<*/
-        }
-        {/*//%= render 'partials/dropdown', open_btn:*/
-        }
-        {/*//current_user.email, items: {:Profile => {controller: 'users', action: 'profile'}}, last_item_text: 'Logout', last_item_path: {controller: 'sessions', action: 'logout'} %>*/
-        }
-        {/*//<*/
-        }
-        {/*//% else %>*/
-        }
-        {/*//<div>*/
-        }
-        {/*//    <*/
-        }
-        {/*//    %= render 'partials/login_modal' %>*/
-        }
-        {/*//    <*/
-        }
-        {/*//    %= render 'partials/register_modal' %>*/
-        }
-        {/*//</div>*/
-        }
-        {/*// <*/
-        }
-        {/*// % end %>*/
-        }
-        {/*}*/
-        }
-        {/*</div>*/
-        }
-        {/*</nav>*/
-        }
-        {/*</div>*/
-        }
     }
 }
 
